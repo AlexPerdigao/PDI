0e3bad50b060ffce846cb4af2515c0b7a8cce0de
Bowie <alexfperdigao@gmail.com>
2024-09-03 20:45:47 -0300

    Merge remote-tracking branch 'origin/main'

--- aluraplay/alterar-video.php
+++ aluraplay/alterar-video.php
@@ -3,6 +3,12 @@
 $dbPath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO("sqlite:$dbPath");
 
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+if ($id === FALSE) {
+    header('Location: /index.php?sucesso=0');
+    exit();
+}
+
 $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
 if ($url === false) {
     header('Location: /index.php?sucesso=0');
@@ -14,17 +20,16 @@ if ($titulo === false) {
     exit();
 }
 
-$sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
+$sql = "UPDATE videos SET url = :url, title = :title WHERE id = :id";
 $stmt = $pdo->prepare($sql);
-$stmt->bindValue(1, $url);
-$stmt->bindValue(2, $titulo);
+$stmt->bindValue(':url', $url);
+$stmt->bindValue(':title', $titulo);
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
 
 if ($stmt->execute() === false) {
     header('Location: /index.php?sucesso=0');
 } else {
     header('Location: /index.php?sucesso=1');
 }
-
-
 
 
