c8cc5529de4a605ba834f3ba51459c45adaae0cd
Bowie <alexfperdigao@gmail.com>
2024-09-08 20:15:59 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ aluraplay/src/Controller/NewVideoController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Alura\Mvc\Controller;
+
+use Alura\Mvc\Entity\Video;
+use Alura\Mvc\Repository\VideoRepository;
+
+class NewVideoController implements Controller
+{
+    public function __construct(private VideoRepository $videoRepository)
+    {
+        $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+        if ($url === false) {
+            header('Location: /?sucesso=0');
+            return;
+        }
+        $titulo = filter_input(INPUT_POST, 'titulo');
+        if ($titulo === false) {
+            header('Location: /?sucesso=0');
+            return;
+        }
+
+        $success = $this->videoRepository->add(new Video($url, $titulo));
+        if ($success === false) {
+            header('Location: /?sucesso=0');
+        } else {
+            header('Location: /?sucesso=1');
+        }
+    }
+
+    public function processaRequisicao(): void
+    {
+        // TODO: Implement processaRequisicao() method.
+    }
+}
